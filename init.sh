ea5795c2b0672a8980d105096ea140108cdfd025
felix <fsafaridis@hotmail.com>
2021-04-04 21:07:57 +0300

    remove current

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for p in 127.0.0.1,10000 127.0.0.1,10001 127.0.0.2,10000 127.0.0.2,10001 127.0.0.3,10000 127.0.0.3,10001;  do IFS=","; set -- $p;
-    ./kvServer -a $1 -p "$2" > /dev/null &
+    kvServer -a $1 -p "$2" > /dev/null &
 done
 
 ps aux | grep kvServer
